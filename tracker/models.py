0b20909e230e3ca23b4a96a5cd4d1353e8d55a78
taylor <taylor@bluestem.solutions>
2025-01-29 11:17:03 -0500

    Completed video #4: Add testing dependencies

--- tracker/models.py
+++ tracker/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from .managers import TransactionQuerySet
 
 class User(AbstractUser):
     pass
@@ -26,6 +26,8 @@ class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     date = models.DateField()
+
+    objects = TransactionQuerySet.as_manager()
 
     def __str__(self):
         return f"{self.type} of {self.amount} on {self.date} by {self.user}"
